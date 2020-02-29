01ce0c51046cc09a5d7cdd29298d8a259f97232a
Obaid <obaidulhaq20@gmail.com>
2020-02-29 22:47:37 +0500

    Updates

---
+++ precache-manifest.ab4a7b2efef7485a738f364c48213d4d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc831004583511b3e40e4d1b87bdfc0f",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "6076c231bd74904ac52c",
+    "url": "/todo-react/static/css/main.cba30053.chunk.css"
+  },
+  {
+    "revision": "961e2731b8f6cd44bbb7",
+    "url": "/todo-react/static/js/2.ca412e35.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/todo-react/static/js/2.ca412e35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6076c231bd74904ac52c",
+    "url": "/todo-react/static/js/main.5324ed32.chunk.js"
+  },
+  {
+    "revision": "2428ea3caf2283ea2d88",
+    "url": "/todo-react/static/js/runtime-main.a22a98c4.js"
+  },
+  {
+    "revision": "04627f3493e30b61b1d623a596de029e",
+    "url": "/todo-react/static/media/Choco Cookies Demo.04627f34.woff"
+  },
+  {
+    "revision": "a52514088216cca4a81d89038de87b73",
+    "url": "/todo-react/static/media/Moontells Demo.a5251408.woff"
+  }
+]);
